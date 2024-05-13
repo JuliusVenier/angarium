2e4be2c263d252b0e5d09b0cdfc1e9e35cb1fbe2
JuliusVenier <julius.venier@gmail.com>
2024-05-13 08:51:59 +0200

    Config angepasst

--- src/main/webui/vite.config.js
+++ src/main/webui/vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		open: true,
+		// this sets a default port to 3000, you can change this
+		port: 3000,
+	}
 });
