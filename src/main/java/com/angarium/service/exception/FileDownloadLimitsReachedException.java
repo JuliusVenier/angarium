de99b157b4253d3e854f0b48be288be18af1bccf
Julius Venier <julius.venier@gmail.com>
2024-06-17 13:32:29 +0200

    Dokumentation

--- src/main/java/com/angarium/service/exception/FileDownloadLimitsReachedException.java
+++ src/main/java/com/angarium/service/exception/FileDownloadLimitsReachedException.java
@@ -1,6 +1,10 @@
 package com.angarium.service.exception;
 
 public class FileDownloadLimitsReachedException extends RuntimeException{
+
+    /**
+     * Diese Ausnahme wird ausgelöst, wenn das Download-Limit oder das Tage-Limit einer Datei erreicht ist.
+     */
     public FileDownloadLimitsReachedException(String msg) {
         super(msg);
     }
