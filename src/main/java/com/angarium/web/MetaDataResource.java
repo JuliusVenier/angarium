88022c7495ffedcb927d89aaef99a55de27afd22
Julius Venier <julius.venier@gmail.com>
2024-06-17 14:33:42 +0200

    Delete für Dateien eingebaut

--- src/main/java/com/angarium/web/MetaDataResource.java
+++ src/main/java/com/angarium/web/MetaDataResource.java
@@ -5,11 +5,14 @@ import com.angarium.model.FileMetaDataModel;
 import com.angarium.service.FileService;
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/api/meta-data")
 @RequiredArgsConstructor
@@ -29,5 +32,13 @@ public class MetaDataResource {
     @RolesAllowed("user")
     public List<FileMetaDataModel> getMyFiles() {
         return fileService.getMyFiles();
+    }
+
+    @DELETE
+    @Path("/{fileId}")
+    @RunOnVirtualThread
+    @RolesAllowed("admin")
+    public void deleteFiles(String fileId) throws IOException {
+        fileService.deleteFile(UUID.fromString(fileId));
     }
 }
